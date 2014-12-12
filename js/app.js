936f088ec031fb92aa5d9a92736be68a0573106e
Gui Bastos <guibastos@gmail.com>
2014-12-11 23:18:31 -0800

    new sounds

--- js/app.js
+++ js/app.js
@@ -1,33 +1,85 @@
 $(document).ready(function() {
-	$('.ryu').mouseenter(function() {
+	$('.ryu')
+		.mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {
-		playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').finish().show()
-		.animate(
-			{'left': '300px'},
-			500,
-			function() {
-				$(this).hide();
-				$(this).css('left', '-212px');
-			}
-		);
-	})
-	.mouseup(function() {
+		
 		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	});
+		$('.hadouken').hide()
+		
+			})
+		.mouseleave(function() {
+			$('.ryu-ready').hide();
+			$('.ryu-still').show();
+		
+			$('.ryu-throwing').hide();
+			$('.hadouken').hide()
+		
+			})
+		.mousedown(function() {
+			playHadouken();
+			$('.ryu-ready').hide();
+			$('.ryu-still').hide();
+			$('.ryu-throwing').show();
+			$('.hadouken').finish().show()
+				.animate(
+					{'left': '300px'},
+					500,
+					function() {
+					$(this).hide();
+					$(this).css('left', '-212px');
+					}
+				);
+			})
+		.mouseup(function() {
+			$('.ryu-throwing').hide();
+			$('.ryu-still').hide();
+			$('.ryu-ready').show();
+
+			$('.ryu-throwing').hide();
+		});
+	
+	$(document)
+		.keydown(function(x){
+			var code = x.keyCode || e.which;
+			if(code == '88'){
+				playDireStraits();
+				$('.ryu-still').hide();
+				$('.ryu-ready').hide();
+				$('.ryu-throwing').hide();
+				$('.ryu-cool').show();
+				x.stopPropagation();
+				
+				}
+			})
+		.keyup(function(x){
+			var code = x.keyCode || e.which;
+			if(code == '88'){
+				pauseDireStraits();
+				$('.ryu-still').show();
+				$('.ryu-ready').hide();
+				$('.ryu-throwing').hide();
+				$('.ryu-cool').hide();
+				x.stopPropagation();
+				}
+			})
 });
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
+function playDireStraits () {
+  $('#relax-sound')[0].volume = 0.5;
+  // $('#relax-sound')[0].load();
+  // $('#relax-sound')[0].prop('currentTime', 0);
+  $('#relax-sound')[0].play();
+}
+
+function pauseDireStraits () {
+  $('#relax-sound')[0].pause();
+  $('#relax-sound')[0].load();
+  // $('#relax-sound').Stop;
+}
+
